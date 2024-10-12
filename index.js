6a86c6e9e23d45a33965489d632d9359156f6887
yuran4ik <shlyapin.y.a@mail.ru>
2024-10-12 21:10:52 +0400

    YS test

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ const port = process.env.PORT || 80
 
 app.use(cors())
 
-// app.use(async (req, res) => {
-//     if (req.query.q == 1){
-//         await prisma.asd.createMany({
-//             data: [
-//             {name: '123'},
-//             {name: 'ss'},
-//             {name: 'asd'},
-//             {name: 'asd'},
-//             ]
-//         })
-//         res.send('created')
-//     } else {
-//         await prisma.asd.findMany()
-//         .then(o => {
-//             res.send(o)
-//         })
-//     }
-// })
+app.use(async (req, res) => {
+    if (req.query.q == 1){
+        await prisma.asd.createMany({
+            data: [
+            {name: '123'},
+            {name: 'ss'},
+            {name: 'asd'},
+            {name: 'asd'},
+            ]
+        })
+        res.send('created')
+    } else {
+        await prisma.asd.findMany()
+        .then(o => {
+            res.send(o)
+        })
+    }
+})
 
 app.listen(port, (err) => {
     console.log(`server started http://localhost:${port}`)
