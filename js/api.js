5e6e45e1ad3e85739e9e9d28d5b04b1da8caf0a3
tersa73 <grigory0707@gmail.com>
2024-10-13 20:28:14 +0400

    CONSTANTA

--- js/api.js
+++ js/api.js
@@ -5,5 +5,33 @@ const prisma = new PrismaClient()
 const router = express.Router()
 
 //начинайте писать свой код здесь
-
+router.post('/News', async (req, res) => {
+    await prisma.News.create({
+        data: req.body
+    })
+        .then(o => {
+            res.status(201).send(o)
+        }) 
+} )
+router.route('/News/:id')
+    .get(async (req, res) => {
+        await prisma.News.findFirst({
+            where: {
+                id: Number(req.params.id)
+            }
+        })
+            .then(o => {
+                res.status(200).send(o)
+            })
+    })
+    .delete(async (req, res) => {
+        await prisma.News.delete({
+            where: {
+                id: Number(rew.params.id)
+            }
+        })
+            .then(o => {
+                res.status(200).send(o)
+            })
+    })
 export default router
